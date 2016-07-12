e286f43733bb79093825c30d2701370b64b6532e
Gregory Giecold <GGiecold@users.noreply.github.com>
2016-07-11 22:26:35 -0400

    Update GiniClust_Main.R

--- src/PyGiniClust/GiniClust_Main.R
+++ src/PyGiniClust/GiniClust_Main.R
@@ -1,5 +1,5 @@
-# Authors: Lan Jiang; Gregory Giecold
-# Contact information: lan_jiang@hms.harvard.edu; ggiecold@jimmy.harvard.edu
+# Authors: Lan Jiang
+# Contact information: lan_jiang@hms.harvard.edu
 
 ############################### work directory ############################### 
 workdir = getwd() 
