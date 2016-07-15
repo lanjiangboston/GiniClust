ad9fe0041382efc8b32be1c23212bf06c8efe25f
Gregory Giecold <GGiecold@users.noreply.github.com>
2016-07-15 15:36:15 -0400

    Update GiniClust_Clustering.R

--- Rfunction/GiniClust_Clustering.R
+++ Rfunction/GiniClust_Clustering.R
@@ -1,3 +1,6 @@
+# Author: Lan Jiang
+# Contact information: lan_jiang@hms.harvard.edu
+
 #################### dbscan for clustering ####################
 GiniClust_Clustering <- function(data.type,ExprM.RawCounts.filter,GeneList.final,eps,MinPts,out.folder,exprimentID){
   
