d12a12debd6b20707579d62d49c3b868d37f2961
Huidong Chen <gjlmile@gmail.com>
2017-03-22 18:36:37 -0400

    Update GiniClust_Filtering.R

--- Rfunction/GiniClust_Filtering.R
+++ Rfunction/GiniClust_Filtering.R
@@ -2,7 +2,7 @@
 # Contact information: lan_jiang@hms.harvard.edu
 
 #################### Filter data ####################
-GiniClust_Filtering <- function(ExprM.RawCounts,ExprM.normCounts,out.folder,exprimentID){
+GiniClust_Filtering <- function(ExprM.RawCounts,out.folder,exprimentID){
   #expressed_cutoff:value lower than this value could be just noise.
   ExpressedinCell_per_gene=apply(ExprM.RawCounts,1,function(x) length(x[x > expressed_cutoff ]))
   nonMir = grep("MIR|Mir", rownames(ExprM.RawCounts), invert = T)  # because Mir gene is usually not accurate 
@@ -10,11 +10,8 @@ GiniClust_Filtering <- function(ExprM.RawCounts,ExprM.normCounts,out.folder,expr
   ExpressedGene_per_cell=apply(ExprM.RawCounts[Genelist,],2,function(x) length(x[x>0]))
   length(Genelist)
   ExprM.RawCounts.filter = ExprM.RawCounts[Genelist,ExpressedGene_per_cell >= minGeneNum]
-  ExprM.normCounts.filter  = ExprM.normCounts[rownames(ExprM.RawCounts.filter), colnames(ExprM.RawCounts.filter)]
   dim(ExprM.RawCounts.filter) 
-  dim(ExprM.normCounts.filter) 
   write.table(ExprM.RawCounts.filter, file=paste(out.folder, "/", exprimentID, "_gene.expression.matrix.RawCounts.filtered.csv", sep=""), sep=",", row.names = TRUE, col.names = TRUE)
-  write.table(ExprM.normCounts.filter,  file=paste(out.folder, "/", exprimentID, "_gene.expression.matrix.normCounts.filtered.csv", sep=""),  sep=",", row.names = TRUE, col.names = TRUE)
-  return(list('raw' = ExprM.RawCounts.filter,'norm' = ExprM.normCounts.filter))
+  return(list('raw' = ExprM.RawCounts.filter))
 }
 ####################      End     ####################
