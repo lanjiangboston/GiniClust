705a91618dadb8bbd6382e5c08ec9e52faea3612
lanjiangboston <lan_jiang@dfci.harvard.edu>
2018-06-18 14:03:23 -0400

    Update GiniClust_packages.R
    Change http to https.

--- Rfunction/GiniClust_packages.R
+++ Rfunction/GiniClust_packages.R
@@ -6,55 +6,55 @@
 #########################################################
 
 if (!suppressWarnings(require("ggplot2",quietly = TRUE))) {
-   install.packages("ggplot2", dependencies = TRUE, repos="http://cran.r-project.org")
+   install.packages("ggplot2", dependencies = TRUE, repos="https://cran.r-project.org")
    }
 if (!suppressWarnings(require("hydroGOF",quietly = TRUE))) {
-   install.packages("hydroGOF", dependencies = TRUE, repos="http://cran.r-project.org")
+   install.packages("hydroGOF", dependencies = TRUE, repos="https://cran.r-project.org")
    }
 if (!suppressWarnings(require("glmnet",quietly = TRUE))) {
-   install.packages("glmnet", dependencies = TRUE, repos="http://cran.r-project.org")
+   install.packages("glmnet", dependencies = TRUE, repos="https://cran.r-project.org")
    }
 if (!suppressWarnings(require("StatMatch",quietly = TRUE))) {
-   install.packages("StatMatch", dependencies = TRUE, repos="http://cran.r-project.org")
+   install.packages("StatMatch", dependencies = TRUE, repos="https://cran.r-project.org")
    }
 if (!suppressWarnings(require("Rtsne",quietly = TRUE))) {
-   install.packages("Rtsne", dependencies = TRUE, repos="http://cran.r-project.org")
+   install.packages("Rtsne", dependencies = TRUE, repos="https://cran.r-project.org")
    }
 if (!suppressWarnings(require("fpc",quietly = TRUE))) {
-   install.packages("fpc", dependencies = TRUE, repos="http://cran.r-project.org")
+   install.packages("fpc", dependencies = TRUE, repos="https://cran.r-project.org")
    }
 if (!suppressWarnings(require("GA",quietly = TRUE))) {
-   install.packages("GA", dependencies = TRUE, repos="http://cran.r-project.org")
+   install.packages("GA", dependencies = TRUE, repos="https://cran.r-project.org")
    }
 if (!suppressWarnings(require("MASS",quietly = TRUE))) {
-   install.packages("MASS", dependencies = TRUE, repos="http://cran.r-project.org")
+   install.packages("MASS", dependencies = TRUE, repos="https://cran.r-project.org")
    }
 if (!suppressWarnings(require("session",quietly = TRUE))) {
-   install.packages("session", dependencies = TRUE, repos="http://cran.r-project.org")
+   install.packages("session", dependencies = TRUE, repos="https://cran.r-project.org")
    }
 if (!suppressWarnings(require("Matrix",quietly = TRUE))) {
-   install.packages("Matrix", dependencies = TRUE, repos="http://cran.r-project.org")
+   install.packages("Matrix", dependencies = TRUE, repos="https://cran.r-project.org")
    }
 if (!suppressWarnings(require("vegan",quietly = TRUE))) {
-   install.packages("vegan", dependencies = TRUE, repos="http://cran.r-project.org")
+   install.packages("vegan", dependencies = TRUE, repos="https://cran.r-project.org")
    }
 if (!suppressWarnings(require("data.table",quietly = TRUE))) {
-   install.packages("data.table", dependencies = TRUE, repos="http://cran.r-project.org")
+   install.packages("data.table", dependencies = TRUE, repos="https://cran.r-project.org")
    }
 if (!suppressWarnings(require("reshape",quietly = TRUE))) {
-   install.packages("reshape", dependencies = TRUE, repos="http://cran.r-project.org")
+   install.packages("reshape", dependencies = TRUE, repos="https://cran.r-project.org")
    }
 if (!suppressWarnings(require("SparseM",quietly = TRUE))) {
-   install.packages("SparseM",dependencies = TRUE, repos="http://cran.r-project.org")
+   install.packages("SparseM",dependencies = TRUE, repos="https://cran.r-project.org")
    }
 if (!suppressWarnings(require("abind",quietly = TRUE))) {
-   install.packages("abind", dependencies = TRUE, repos="http://cran.r-project.org")
+   install.packages("abind", dependencies = TRUE, repos="https://cran.r-project.org")
    }
 if (!suppressWarnings(require("drc",quietly = TRUE))) {
-   install.packages("drc", dependencies = TRUE, repos="http://cran.r-project.org")
+   install.packages("drc", dependencies = TRUE, repos="https://cran.r-project.org")
    }
 if (!suppressWarnings(require("gplots",quietly = TRUE))) {
-   install.packages("gplots",dependencies = TRUE, repos="http://cran.r-project.org")
+   install.packages("gplots",dependencies = TRUE, repos="https://cran.r-project.org")
 }
 
 
